712bf1a929e81ce4bd2554cd79152cb5f62ae4df
MarceloMax50 <49374397+MarceloMax50@users.noreply.github.com>
2020-04-02 22:52:04 -0300

    Controller Class Para exibir os Menus
    Menu Principal

--- src/main/java/ConsoleTest/Main.java
+++ src/main/java/ConsoleTest/Main.java
@@ -2,11 +2,19 @@ package ConsoleTest;
 
 import Application.PatientApp;
 import Domain.Model.Users.Patient;
+import Domain.Controller.Menu.Cmenu;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Cmenu m = new Cmenu ();
+        Stage stage = new Stage();
+        m.RunMenu("MenuPrincipalView.fxml", stage);
+
+
         System.out.println("Started");
         PatientApp userApp = new PatientApp();
         Patient patient = new Patient();
