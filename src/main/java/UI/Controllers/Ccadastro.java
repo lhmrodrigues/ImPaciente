ce77e11f45ca401f3f69c02c6b14bef57a3af80f
Douglas Lopes <douglas_lopes.s@hotmail.com>
2020-04-08 20:34:42 -0300

    Cadastro

--- src/main/java/UI/Controllers/Ccadastro.java
+++ src/main/java/UI/Controllers/Ccadastro.java
@@ -2,40 +2,61 @@ package UI.Controllers;
 
 import Application.MedicApp;
 import Application.PatientApp;
+import Application.UserSession;
 import Domain.Model.Users.Medic;
 import Domain.Model.Users.Patient;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class Ccadastro {
+public class Ccadastro extends CBase{
 
     public TextField txtNome;
-    public CheckBox checkBox;
     public TextField txtCpfCrm;
+    public TextField txtSenha;
     public Button btnCadastrar;
 
     public void BtnCadastroClick(ActionEvent actionEvent) throws Exception {
-        boolean ifDoctor = checkBox.isSelected();
         String nome = txtNome.getText();
         String doc = txtCpfCrm.getText();
-        if(ifDoctor)
-        {
-            MedicApp save = new MedicApp();
-            Medic novoUsuario = new Medic();
-            novoUsuario.setCpfCrm(doc);
-            novoUsuario.setName(nome);
-            save.Add(novoUsuario);
+        String senha = txtSenha.getText();
+
+        if (doc.isBlank() || senha.isBlank() || nome.isBlank()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Usuário Inválido");
+            alert.setHeaderText("Usuário e/ou Senha e/ou Nome em Branco");
+            alert.show();
+            return;
         }
-        else
+
+        if(doc.length() == 11)
         {
             PatientApp save = new PatientApp();
             Patient novoUsuario = new Patient();
             novoUsuario.setCpfCrm(doc);
             novoUsuario.setName(nome);
+            novoUsuario.setPassword(senha);
             save.Add(novoUsuario);
-        }
 
+            Stage primaryStage = new Stage();
+            UserSession.getInstance().setUserLogado(novoUsuario);
+            OpenPage("InfoReceitaView.fxml", primaryStage);
+        }
+        else
+        {
+            MedicApp save = new MedicApp();
+            Medic novoUsuario = new Medic();
+            novoUsuario.setCpfCrm(doc);
+            novoUsuario.setName(nome);
+            novoUsuario.setPassword(senha);
+            save.Add(novoUsuario);
+
+            Stage primaryStage = new Stage();
+            UserSession.getInstance().setUserLogado(novoUsuario);
+            OpenPage("InfosDoctorView.fxml", primaryStage);
+        }
     }
 }
