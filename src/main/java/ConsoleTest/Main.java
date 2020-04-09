cf753e33c4052e66b12034eb031209438a9cd72b
Igor <igor.lacivita@hotmail.com>
2020-04-09 20:20:26 -0300

    Testes criados - ConsoleTest

--- src/main/java/ConsoleTest/Main.java
+++ src/main/java/ConsoleTest/Main.java
@@ -1,8 +1,15 @@
 package ConsoleTest;
 
+import Application.MedicApp;
+import Application.MedicineApp;
 import Application.PatientApp;
+import Application.PrescriptionApp;
+import Domain.Model.Medicine.Medicine;
+import Domain.Model.Prescription.Prescription;
+import Domain.Model.Users.Medic;
 import Domain.Model.Users.Patient;
 
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -13,8 +20,39 @@ public class Main {
         patient.setCpfCrm("42188612850");
         patient.setName("Douglas");
         userApp.Add(patient);
-        List<Patient> patientList = userApp.getAll(Patient.class);
+        java.util.List<Patient> patientList = userApp.getAll(Patient.class);
 
         System.out.println(patientList.get(0).id);
+
+        MedicApp medicApp = new MedicApp();
+        Medic medic = new Medic();
+        medic.setCpfCrm("1234");
+        medic.setName("Luis");
+        medic.setPatientList(patientList);
+        medicApp.Add(medic);
+        java.util.List<Medic> medicList = medicApp.getAll(Medic.class);
+
+        System.out.println(medicList.get(0).id);
+
+        MedicineApp medicineApp = new MedicineApp();
+        Medicine medicine = new Medicine();
+        medicine.setName("Dramin");
+        medicineApp.Add(medicine);
+        java.util.List<Medicine> medicineList = medicineApp.getAll(Medicine.class);
+
+        System.out.println(medicineList.get(0).id);
+
+        Date date = new Date();
+        PrescriptionApp prescriptionApp = new PrescriptionApp();
+        Prescription prescription = new Prescription();
+        prescription.setPatient(patient);
+        prescription.setMedic(medic);
+        prescription.setDateOfInclude(date);
+        prescription.setMedicineList(medicineList);
+        prescriptionApp.Add(prescription);
+        List<Prescription> prescriptionList = prescriptionApp.getAll(Prescription.class);
+
+        System.out.println(prescriptionList.get(0).id);
+
     }
 }
